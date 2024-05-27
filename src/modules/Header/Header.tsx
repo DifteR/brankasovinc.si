5a5f897701a55600f5ca26790096d2074cfaaed4
Tilen Marš <its0black0magic@gmail.com>
2024-05-27 19:00:13 +0200

    Add header and menu files

---
+++ src/modules/Header/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+import styles from './Header.module.css'
+import Menu from "../Menu/Menu"
+
+export default function Header(){
+    return <header><Menu></Menu></header>
+}
